2d8d31f5fe6b099a21e79dd34d287f249c7f97bc
Atp <rkre@paypalcorp.con>
2017-10-08 14:25:00 -0700

    Refactor the code base
    Refactor and remove the unwanted files.

--- app.js
+++ app.js
@@ -13,16 +13,7 @@ mongoose.connect('mongodb://localhost/NorthWesternMutual')
 
 var index = require('./routes/index');
 var api = require('./routes/api');
-
 var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -43,5 +34,4 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   res.send({'error':err});
 });
-
 module.exports = app;
